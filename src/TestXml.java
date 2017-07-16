ba6a3c10ac60536cf8bba5166edc67765ac85d2d
leol <654977767@qq.com>
2017-07-16 22:12:38 +0800

    uuu

---
+++ src/TestXml.java
@@ -0,0 +1,10 @@
+import util.XmlParser;
+
+public class TestXml {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		XmlParser.createEmptyXmlFile("d:/haha.xml");
+	}
+
+}
